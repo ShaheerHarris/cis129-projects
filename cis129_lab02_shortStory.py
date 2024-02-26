250d4b7c4e08935e53cc16c7c1a4900e846ebc67
ShaheerHarris <161271161+ShaheerHarris@users.noreply.github.com>
2024-02-25 21:29:22 -0700

    Added Shaheer's story to cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print ("Hello, World!")
+#Shaheer's Harris
+# My first python lab in CIS129
+#Prints simple story
+print ("Shaheer found a bucket of gold among the sea")
+print ("Pirates spotted Shaheer's boat and aboarded his ship")
+print ("The pirates took his gold and his ship and threw him into the sea")
