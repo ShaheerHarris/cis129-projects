05e4d00d9fd971d82f3aa879e7e4e1c3a5c361e5
ShaheerHarris <161271161+ShaheerHarris@users.noreply.github.com>
2024-03-06 23:35:28 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,13 +1,15 @@
 print("My Coffee and Muffin Shop")
 
-def calculate_total_cost(coffee_amount, muffin_amount):
+def calculate_total_cost(coffee_amount, muffin_amount, boba_amount, tea_amount):
     # Prices
     coffee_price = 5
     muffin_price = 4
+    donut_price = 2
+    tea_price = 3
     tax_rate = 0.06
 
     # Subtotal
-    subtotal = (coffee_price * coffee_amount) + (muffin_price * muffin_amount) 
+    subtotal = (coffee_price * coffee_amount) + (muffin_price * muffin_amount) + (donut_price * donut_amount) + (tea_price * tea_amount)
 
     # Taxes
     tax_amount = (subtotal * tax_rate)
@@ -20,10 +22,11 @@ def calculate_total_cost(coffee_amount, muffin_amount):
 # Order
 coffee_amount = int(input("Enter the number of coffees: "))
 muffin_amount = int(input("Enter the number of muffins: "))
-
+donuts_amount = int(input("Enter the number of donuts: "))
+tea_amount = int(input("Enter the number of teas: "))
 # Total cost 
-total_cost = calculate_total_cost(coffee_amount, muffin_amount)
+total_cost = calculate_total_cost(coffee_amount, muffin_amount, donut_amount, tea_price)
 
 # Results
-print("\nTotal Cost: $", total_cost)
+print("\nTotal Cost: $", total_cost, "\n\nThank you for shopping with us!")
 
